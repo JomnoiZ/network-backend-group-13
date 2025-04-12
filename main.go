a39d6bcc1ea90d37e94b515a6f0f68551ef2f7ab
Vijak Khajornritdacha <vijak13019@hotmail.com>
2025-04-12 14:29:12 +0700

    feat: add cors

--- main.go
+++ main.go
@@ -12,10 +12,26 @@ import (
 func main() {
 	websocketService := services.NewWebsocketService()
 
-	routes.WebsocketRoute(websocketService)
+	cors := func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
-	// Serve frontend from /public
-	http.Handle("/", http.FileServer(http.Dir("./public")))
+			if r.Method == http.MethodOptions {
+				w.WriteHeader(http.StatusOK)
+				return
+			}
+			next.ServeHTTP(w, r)
+		})
+	}
+
+	// Apply CORS to WebSocket route
+	routes.WebsocketRoute(websocketService, cors)
+
+	// Serve static files with CORS
+	fs := http.FileServer(http.Dir("./public"))
+	http.Handle("/", cors(fs))
 
 	fmt.Println("Server running on PORT 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
