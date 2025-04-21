67449d06e8540f3a90d2448a4d8d30742c7b5f5e
JomnoiZ <kamanun.m@gmail.com>
2025-04-22 01:06:45 +0700

    feat: get all groups api

--- routes/group.routes.go
+++ routes/group.routes.go
@@ -7,16 +7,17 @@ import (
 )
 
 func GroupRoute(r *gin.Engine, groupService services.GroupService) {
-    groupController := controllers.NewGroupController(groupService)
+	groupController := controllers.NewGroupController(groupService)
 
-    rgu := r.Group("/groups")
-    {
-        rgu.GET("/:id", groupController.GetGroup)
-        rgu.POST("/", groupController.CreateGroup)
-        rgu.POST("/:id/members", groupController.AddMember)
-        rgu.DELETE("/:id/members/:username", groupController.KickMember)
-        rgu.POST("/:id/admins", groupController.AddAdmin)
-        rgu.DELETE("/:id/admins/:username", groupController.RemoveAdmin)
-        rgu.GET("/:id/messages", groupController.GetGroupMessages)
-    }
+	rgu := r.Group("/groups")
+	{
+		rgu.GET("", groupController.GetAllGroups)
+		rgu.GET("/:id", groupController.GetGroup)
+		rgu.POST("/", groupController.CreateGroup)
+		rgu.POST("/:id/members", groupController.AddMember)
+		rgu.DELETE("/:id/members/:username", groupController.KickMember)
+		rgu.POST("/:id/admins", groupController.AddAdmin)
+		rgu.DELETE("/:id/admins/:username", groupController.RemoveAdmin)
+		rgu.GET("/:id/messages", groupController.GetGroupMessages)
+	}
 }
